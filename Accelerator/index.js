21af77e7ccb508c759708410a6a0a94aaef3fb30
MaheshMuppasani <mahesh.muppasani007@gmail.com>
2023-07-31 22:23:35 -0400

    fixed minor bugs

--- Accelerator/index.js
+++ Accelerator/index.js
@@ -1,6 +1,7 @@
 const gasPedal = document.getElementById('gad-pedal');
 const brakePedal = document.getElementById('brake-pedal');
 const digitalSpeed = document.getElementById('digitalSpeed');
+const controls = document.getElementsByClassName('controls')[0];
 const speedIndicator = document.querySelector('#sc');
 const carStartAudio = document.getElementById('car-start');
 const carAccelerateAudio = document.getElementById('car-accelerate');
@@ -8,9 +9,9 @@ let accelerateInterval;
 let descelerateInterval;
 let speed = 0;
 
-const renderSpeed = (speed, options = { speedRender : true }) => {
+const renderSpeed = (speed, options = { digitalSpeed : true }) => {
     speedIndicator.style.transform = `rotateZ(${speed*0.675}deg)`; // 0-400 in a 270deg scale = 0.765
-    if(options.speedRender){
+    if(options.digitalSpeed){
         digitalSpeed.textContent = speed;
     }
 }
@@ -20,13 +21,40 @@ const stopAccelerationSound = () => {
     carAccelerateAudio.currentTime = 2;
 }
 
-const accelerateHelper = (maxSpeed, interval, nextStage, options = {}) => {
-    const { step = 1, speedRender = true } = options;
+const slowDown = () => {
+    stopAccelerationSound();
+    clearInterval(accelerateInterval);
+    clearInterval(descelerateInterval);
+    descelerateInterval = setInterval(() => {
+        if(!speed) return clearInterval(descelerateInterval);
+        speed -=1;
+        renderSpeed(speed);
+    }, 50);
+}
+
+const descelerate = (e = null, options = {}, callback = null) => {
+    const { rate = 15, step = 1, digitalSpeed = true} = options;
+    stopAccelerationSound();
+    clearInterval(accelerateInterval);
+    clearInterval(descelerateInterval);
+    descelerateInterval = setInterval(() => {
+        if(speed<=0) {
+            speed = 0
+            clearInterval(descelerateInterval);
+            return callback && callback()
+        }        
+        speed -= step;
+        renderSpeed(speed, {digitalSpeed});
+    }, rate); //23
+}
+
+const accelerateHelper = (maxSpeed, interval, nextGear, options = {}) => {
+    const { step = 1, digitalSpeed = true } = options;
     clearInterval(accelerateInterval);
     accelerateInterval = setInterval(() => {
-        if(speed >= maxSpeed) return (nextStage && nextStage());
+        if(speed >= maxSpeed) return (nextGear && nextGear());
         speed += step;
-        renderSpeed(speed, {speedRender});
+        renderSpeed(speed, {digitalSpeed});
     }, interval)
 }
 
@@ -60,78 +88,56 @@ const accelerate = () => {
         default : return;
     }
 }
-const slowDown = () => {
-    stopAccelerationSound();
-    clearInterval(accelerateInterval);
-    clearInterval(descelerateInterval);
-    descelerateInterval = setInterval(() => {
-        if(!speed) return clearInterval(descelerateInterval);
-        speed -=1;
-        renderSpeed(speed);
-    }, 50);
-}
 
-const descelerate = (e = null, options = {}) => {
-    const { rate = 15, step = 1, speedRender = true} = options;
-    stopAccelerationSound();
-    clearInterval(accelerateInterval);
-    clearInterval(descelerateInterval);
-    descelerateInterval = setInterval(() => {
-        if(speed<=0) {
-            speed = 0
-            return clearInterval(descelerateInterval);
-        }        speed -= step;
-        renderSpeed(speed, {speedRender});
-    }, rate); //23
-}
-
-// Dom element interation
-gasPedal.addEventListener('mousedown', accelerate);
-gasPedal.addEventListener('touchstart', accelerate);
-
-gasPedal.addEventListener('mouseup', slowDown);
-gasPedal.addEventListener('touchend', slowDown);
-
-brakePedal.addEventListener('mousedown', descelerate);
-brakePedal.addEventListener('touchstart', descelerate);
-
-brakePedal.addEventListener('mouseup', slowDown);
-brakePedal.addEventListener('touchend', slowDown);
-
-
-// keyboard interaction
 let keyDown = false, keyUp = false;
+function setControls(){
 
-window.addEventListener('keydown', (e) => {
-    if(!["ArrowUp", "Space", "ArrowDown"].includes(e.code)) return;
-    keyUp = false;
-    if(!keyDown){
-        keyDown = true;
-        if(e.code==="ArrowUp"){
-            gasPedal.classList.add('active');
-            return accelerate();
+    controls.style.display = 'flex'
+    
+    // Dom element interation
+    gasPedal.addEventListener('mousedown', accelerate);
+    gasPedal.addEventListener('touchstart', accelerate);
+
+    gasPedal.addEventListener('mouseup', slowDown);
+    gasPedal.addEventListener('touchend', slowDown);
+
+    brakePedal.addEventListener('mousedown', descelerate);
+    brakePedal.addEventListener('touchstart', descelerate);
+
+    brakePedal.addEventListener('mouseup', slowDown);
+    brakePedal.addEventListener('touchend', slowDown);
+
+    // keyboard interaction
+    window.addEventListener('keydown', (e) => {
+        if(!["ArrowUp", "Space", "ArrowDown"].includes(e.code)) return;
+        keyUp = false;
+        if(!keyDown){
+            keyDown = true;
+            if(e.code==="ArrowUp"){
+                gasPedal.classList.add('active');
+                return accelerate();
+            }
+            else if(["ArrowDown", "Space"].includes(e.code)){
+                brakePedal.classList.add('active');
+                return descelerate();
+            }
         }
-        else if(["ArrowDown", "Space"].includes(e.code)){
-            brakePedal.classList.add('active');
-            return descelerate();
+    });
+    window.addEventListener('keyup', (e) => {
+        keyDown = false;
+        if(!keyUp){
+            keyUp = true;
+            gasPedal.classList.remove('active');
+            brakePedal.classList.remove('active');
+            return slowDown();
         }
-    }
-});
-window.addEventListener('keyup', (e) => {
-    keyDown = false;
-    if(!keyUp){
-        keyUp = true;
-        gasPedal.classList.remove('active');
-        brakePedal.classList.remove('active');
-        return slowDown();
-    }
-});
+    });
+}
 
 window.addEventListener('DOMContentLoaded', () => {
+    controls.style.display = 'none'
     setTimeout(() => {
-        const options = {step: 2.2, rate: 0, speedRender: false};
-        accelerateHelper(400, 0, () => {
-            descelerate(null, options)
-        }, options);    
+        const options = {step: 4, rate: 7, digitalSpeed: false};
+        accelerateHelper(400, 7, () => descelerate(null, options, setControls), options); 
     }, 800)
 })
