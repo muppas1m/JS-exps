a8de31a4b47e5356c9d1d7a93e8c96b8fe9311cc
MaheshMuppasani <mahesh.muppasani007@gmail.com>
2023-07-31 23:21:46 -0400

    This file consists of later work

---
+++ New stuff/new stuff.js
@@ -0,0 +1,11 @@
+// 1. 2048 game
+// 2. speedo meter
+// 3. pattern Lock
+// 4. chess multiplayer
+// 5. interview helper
+// 6. React component render Animation
+// 7. API mocker
+// 8. snake js
+// 9. floating icon Animation
+// 10. words attack typing game
+// 11. z-index object depth in water/jelly - object lift up and down 
