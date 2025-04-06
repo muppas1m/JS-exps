994091f5bfda1f68750e68f798d5884bbff1600a
MaheshMuppasani <mahesh.muppasani007@gmail.com>
2025-04-05 22:57:05 -0400

    fixed audio issue in Ios

--- Accelerator/carFx.js
+++ Accelerator/carFx.js
@@ -1,3 +1,34 @@
+(function() { // IOS fix for audio context issue
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	if (window.AudioContext) {
+		window.audioContext = new window.AudioContext();
+	}
+	var fixAudioContext = function (e) {
+		if (window.audioContext) {
+			// Create empty buffer
+			var buffer = window.audioContext.createBuffer(1, 1, 22050);
+			var source = window.audioContext.createBufferSource();
+			source.buffer = buffer;
+			// Connect to output (speakers)
+			source.connect(window.audioContext.destination);
+			// Play sound
+			if (source.start) {
+				source.start(0);
+			} else if (source.play) {
+				source.play(0);
+			} else if (source.noteOn) {
+				source.noteOn(0);
+			}
+		}
+		// Remove events
+		document.removeEventListener('touchstart', fixAudioContext);
+		document.removeEventListener('touchend', fixAudioContext);
+	};
+	// iOS 6-8
+	document.addEventListener('touchstart', fixAudioContext);
+	// iOS 9
+	document.addEventListener('touchend', fixAudioContext);
+})();
 export class SoundManager {
   constructor() {
     this.ctx = new (window.AudioContext || window.webkitAudioContext)();
